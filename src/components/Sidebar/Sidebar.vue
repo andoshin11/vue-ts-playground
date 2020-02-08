c6a655f08c69bae42910c87705c8a5b221caf859
andoshin11 <shinglish11@gmail.com>
2020-02-08 11:47:28 +0900

    Update Sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="Sidebar" class="Sidebar" :class="{ hidden: isHidden }" :style="innerStyleObj">
-
+    <Tabs :plugins="state.plugins" :currentPluginId="state.currentPlugin && state.currentPlugin.id" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { RootState } from '@/store'
+import Tabs from '@/components/SidebarTabs'
 
 export default Vue.extend({
+  components: {
+    Tabs
+  },
   props: {
     styleObj: {
       type: Object
@@ -21,6 +26,9 @@ export default Vue.extend({
       const defaultStyle = this.styleObj || {}
 
       return defaultStyle
+    },
+    state(): RootState {
+      return this.$store.state
     }
   }
 })
