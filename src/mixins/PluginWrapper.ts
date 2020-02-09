cc5bab7dfc59d8ff8e090a39c22f3e062590b8c0
andoshin11 <shinglish11@gmail.com>
2020-02-09 23:21:55 +0900

    Add PluginWrapper

---
+++ src/mixins/PluginWrapper.ts
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import { RootState } from '@/store'
+
+export default Vue.extend({
+  computed: {
+    state(): RootState {
+      return this.$store.state
+    },
+    modelChangedAt(): RootState['modelChangedAt'] {
+      return this.state.modelChangedAt
+    },
+    modelChangedAtDebouncing(): RootState['modelChangedAtDebouncing'] {
+      return this.state.modelChangedAtDebouncing
+    }
+  },
+  watch: {
+    modelChangedAt: {
+      immediate: true,
+      handler(val: number | null) {
+        // console.log('Invoking wrapper handler')
+        const { $pluginHooks } = this
+        const { sandbox } = this.state
+        if (!sandbox) return
+
+        if (!val) return
+        // Invoke update function
+        if ($pluginHooks && $pluginHooks.modelChanged) {
+          $pluginHooks.modelChanged(sandbox, sandbox.getModel(), this)
+        }
+
+        // FIXME: Handle debouncing
+        // Invoke debounce function
+
+        if ($pluginHooks && $pluginHooks.modelChangedDebounce) {
+          $pluginHooks.modelChangedDebounce(sandbox, sandbox.getModel(), this)
+        }
+      }
+    },
+  },
+  mounted() {
+    // register pluginHooks as component method
+    const { pluginHooks } = (this.$options as any).__proto__
+    if (!pluginHooks) return
+
+    // @ts-ignore
+    this['$pluginHooks'] = pluginHooks
+  }
+})
