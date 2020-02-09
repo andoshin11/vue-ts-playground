002a5a929dd0b85a3a53d067523affae3b22a2f0
andoshin11 <shinglish11@gmail.com>
2020-02-09 23:10:40 +0900

    Add sandbox const

---
+++ src/lib/sandbox/const.ts
@@ -0,0 +1,25 @@
+import { PlaygroundConfig } from './types'
+
+/** Default Monaco settings for playground */
+export const sharedEditorOptions: import("monaco-editor").editor.IEditorOptions = {
+  automaticLayout: true,
+  scrollBeyondLastLine: true,
+  scrollBeyondLastColumn: 3,
+  minimap: {
+    enabled: false
+  }
+};
+
+/** The default settings which we apply a partial over */
+export const defaultPlaygroundSettings: PlaygroundConfig = {
+  text: "",
+  domID: "",
+  compilerOptions: {},
+  acquireTypes: true,
+  useJavaScript: false,
+  supportTwoslashCompilerOptions: false,
+  logger: {
+    error: () => {},
+    log: () => {}
+  }
+}
