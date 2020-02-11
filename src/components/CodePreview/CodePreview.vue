a78b23f9f9a935b087b08559981f8dc5c3102a3c
andoshin11 <shinglish11@gmail.com>
2020-02-11 10:14:46 +0900

    Add CodePreview component

---
+++ src/components/CodePreview/CodePreview.vue
@@ -0,0 +1,38 @@
+<template>
+  <pre class="pre">
+    <code class="code" v-html="content"></code>
+  </pre>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    content: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style scoped>
+.pre {
+  background-color: #f5f5f5;
+  width: 100%;
+  position: relative;
+}
+
+.code {
+  padding: 16px 0;
+  width: calc(100% - 18px);
+  height: 100%;
+  overflow-x: scroll;
+  box-shadow: none;
+}
+
+.code::before {
+  content: ''
+}
+</style>
