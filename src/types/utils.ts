2a6914d76b2dff72c893d2bb0adf84df2fb0dd4b
andoshin11 <shinglish11@gmail.com>
2020-02-11 17:57:46 +0900

    Add Primitive type

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,1 +1,10 @@
 export type TupleToUnion<T> = T extends { [K in keyof T]: infer U } ? U : never;
+
+export type Primitive =
+  | string
+  | number
+  | bigint
+  | boolean
+  | symbol
+  | null
+  | undefined;
