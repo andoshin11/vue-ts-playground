827d61aa837dad64911f46c9463611f1158c77dc
andoshin11 <shinglish11@gmail.com>
2020-04-15 00:40:39 +0900

    Add sortBy helper

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,10 @@ export * from "./monaco"
 export function isVueComponentRef(node: Vue | Element | Vue[] | Element[]): node is Vue {
   return !!node && '__vue__' in node
 }
+
+export const sortBy = <T extends object>(key: keyof T) => (a: T, b: T) => {
+  const keyOfA = a[key]
+  const keyofB = b[key]
+
+  return keyOfA === keyofB ? 0 : keyOfA > keyofB ? 1 : -1
+}
